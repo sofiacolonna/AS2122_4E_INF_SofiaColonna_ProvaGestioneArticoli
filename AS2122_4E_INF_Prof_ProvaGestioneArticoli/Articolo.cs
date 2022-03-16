90529b17ad5e13345862029b2f4329c849096201
Sofia Colonna <sofcolonna04@gmail.com>
2022-03-16 09:57:12 +0100

    Punto 10: impostazione costruttore classe Articolo come utilizzato da frmMain

--- AS2122_4E_INF_Prof_ProvaGestioneArticoli/Articolo.cs
+++ AS2122_4E_INF_Prof_ProvaGestioneArticoli/Articolo.cs
@@ -10,23 +10,27 @@ namespace AS2122_4E_INF_Prof_ProvaGestioneArticoli
     {
         // TODO: (8) aggiungere attributi privati di Articolo
         // ...
-        double prezzo;
         int codice;
+        double prezzo;
         string descrizione;
         string unitaDiMisura;
         
 
         // TODO: (9) aggiungere property di sola lettura dei dati necessari all'esterno di Articolo
         // ...
-        public Articolo(int codice, string descrizione, string unitaMisura, double prezzo)
+        public Articolo(int codice, string descrizione, string unitaDiMisura, double prezzo)
         {
             // TODO: (10) impostazione costruttore classe Articolo come utilizzato da frmMain
             // ...
+            this.prezzo = prezzo  ;
+            this.descrizione = descrizione;
+            this.unitaDiMisura = unitaDiMisura;
+            
         }
 
         public string Visualizzati()
         {
-            return $"{descrizione} {unitaMisura} {prezzo} ({codice})";
+            return $"{descrizione} {unitaDiMisura} {prezzo} ({codice})";
         }
     }
 }
