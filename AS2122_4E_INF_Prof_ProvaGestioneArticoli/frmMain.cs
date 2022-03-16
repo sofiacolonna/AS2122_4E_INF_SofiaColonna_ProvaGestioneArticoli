4f50b71159814c80482bcfb7591a9edf43df0f59
Sofia Colonna <sofcolonna04@gmail.com>
2022-03-16 10:20:14 +0100

    (2) aggiungere visualizzazione articoli inseriti nella listbox

--- AS2122_4E_INF_Prof_ProvaGestioneArticoli/frmMain.cs
+++ AS2122_4E_INF_Prof_ProvaGestioneArticoli/frmMain.cs
@@ -52,6 +52,10 @@ namespace AS2122_4E_INF_Prof_ProvaGestioneArticoli
                 case "Visualizza articoli":
                     // TODO: (2) aggiungere visualizzazione articoli inseriti nella listbox
                     // ... lstElenco.Items.Add(a.Visualizzati()); 
+                    foreach (Articolo a in articoli)
+                    {
+                        lstElenco.Items.Add(a.Visualizzati());
+                    }
                     break;
             }
         }
